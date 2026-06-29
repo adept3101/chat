8d5aa4b30611dfc6529c91de8b2e605ece946e8f
adept <timofeysitnikov@mail.ru>
2026-06-29 13:53:56 +0300

    upd

--- src/msg.c
+++ src/msg.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <time.h>
 
 void *send_msg(void *arg) {
   thread_data *data = (thread_data *)arg;
@@ -37,8 +38,26 @@ void *get_msg(void *arg) {
   return NULL;
 }
 
-void log_msg(char buff[32]){
+void log_msg(const char buff[32]){
+  time_t time_now = time(NULL);
+  struct tm *now = localtime(&time_now);
+
+  log_data ld;
+
+  char str_t[20];
+  char str_d[20];
+
+  strftime(str_t, sizeof(str_t),"%T", now);
+  strftime(str_d, sizeof(str_d),"%D", now);
+  // snprintf(ld.time, 64, "%s %s", str_t, str_d);
+
   FILE *fp = fopen("log.txt", "a");
-  fputs(buff, fp);
+  if(fp ==  NULL){
+    perror("Error open file\n");
+    return;
+  }
+
+  snprintf(ld.buff, 128, "%s %s %s", buff, str_t, str_d);
+  fputs(ld.buff, fp);
   fclose(fp);
 }
--- src/msg.h
+++ src/msg.h
@@ -1,13 +1,18 @@
 #ifndef MSG_H
 #define MSG_H
+#include <time.h>
 
 void* get_msg(void *arg);
 void* send_msg(void *arg);
-void log_msg(char buff[32]);
+void log_msg(const char buff[32]); //const потому что char[] эквивалентен *char
 
 typedef struct {
   int sock;
   char name[32];
 } thread_data;
+
+typedef struct {
+  char buff[128];
+} log_data;
 
 #endif // MSG_H
